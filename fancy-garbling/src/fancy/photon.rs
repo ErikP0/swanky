70be54bcf40b0c475c0f036a4f15219f1fb47a5e
Robbe Vermeiren <vermeirenr@example.com>
2022-08-18 15:44:14 +0200

    Implemented MixColumnsSerial function for photon

--- fancy-garbling/src/fancy/photon.rs
+++ fancy-garbling/src/fancy/photon.rs
@@ -14,7 +14,7 @@ use serde_json::de;
 use std::{ops::Index, convert::TryInto};
 
 /// A collection of wires for the PHOTON permutation, useful for the garbled gadgets defined by `PhotonGadgets`.
-// [[W; D]; D] is organized in column-major order
+// [[W; D]; D] is organized in row-major order
 #[derive(Clone)]
 pub struct PhotonState<W> {
     state_matrix: Vec<Vec<W>>,
@@ -36,11 +36,11 @@ impl<W> PhotonState<W>
     /// Create a new PhotonState matrix from an ordered element array.
     pub fn from_vec(w_vec: Vec<W>, d: usize) -> PhotonState<W> {
         assert_eq!(w_vec.len(), d*d);
-        let mut ws: Vec<Vec<W>>;
+        let mut ws: Vec<Vec<W>> = Vec::with_capacity(d*d);
 
-        for c in 0..d {
-            for r in 0..d {
-                ws[c][r] = w_vec[c*d + r];
+        for r in 0..d {
+            for c in 0..d {
+                ws[r][c] = w_vec[r*d + c].clone();
             }
         }
 
@@ -77,12 +77,12 @@ impl<W> PhotonState<W>
         self.state_matrix[col_index][row_index].clone()
     }
 
-    /// Insert a column in the matrix at given `col_index`
-    pub fn insert(&mut self, col_index: usize, col: Vec<W>) {
-        self.state_matrix.insert(col_index, col);
+    /// Insert a row in the matrix at given `row_index`
+    pub fn insert(&mut self, row_index: usize, row: Vec<W>) {
+        self.state_matrix.insert(row_index, row);
     }
 
-    /// Access the underlying iterator over the columns
+    /// Access the underlying iterator over the rows
     pub fn iter(&self) -> std::slice::Iter<Vec<W>> {
         self.state_matrix.iter()
     }
@@ -92,12 +92,12 @@ impl<F: Fancy> PhotonGadgets for F {}
 
 /// Extension trait for Fancy which provides Photon constructions
 pub trait PhotonGadgets: Fancy {
-    fn AddConstants (
+    fn AddConstants<'a> (
         &mut self,
-        state: &mut PhotonState<Self::Item>,
+        state: &'a mut PhotonState<Self::Item>,
         rc: u16,
         ics: &[u16] //hardcode!
-    ) -> Result<&PhotonState<Self::Item>, Self::Error> {
+    ) -> Result<&'a PhotonState<Self::Item>, Self::Error> {
         let w_ics = ics
         .iter()
         .map(|ic| self.constant(*ic, &state.modulus()).unwrap())
@@ -105,32 +105,32 @@ pub trait PhotonGadgets: Fancy {
         let w_rc = self.constant(rc, &state.modulus())?;
         let d = state.dim();
 
-        let first_col = state.state_matrix()[0]
-        .iter()
-        .zip(w_ics.iter())
-        .map(|(cell, iconst)| {
-            let ic_add = self.add(cell, iconst).unwrap();
-            self.add(&ic_add, &w_rc).unwrap()
-        })
-        .collect_vec();
+        // let first_col = state.state_matrix()[0]
+        // .iter()
+        // .zip(w_ics.iter())
+        // .map(|(cell, iconst)| {
+        //     let ic_add = self.add(cell, iconst).unwrap();
+        //     self.add(&ic_add, &w_rc).unwrap()
+        // })
+        // .collect_vec();
 
-        state.state_matrix().insert(0, first_col);
+        state.insert(0, first_col);
 
         Ok(state)
     }
 
-    fn SubCells (
+    fn SubCells<'a> (
         &mut self,
-        state: &mut PhotonState<Self::Item>,
+        state: &'a mut PhotonState<Self::Item>,
         sbox: Vec<u16>,
-    ) -> Result<&PhotonState<Self::Item>, Self::Error> {
+    ) -> Result<&'a PhotonState<Self::Item>, Self::Error> {
         debug_assert_eq!(state.size(), sbox.len(), "Sbox has incorrect dimensions");
 
         let state_mod = state.modulus();
         let mut res_state = state.state_matrix().clone();
-        for col in res_state.iter_mut() {
-            for el in col.iter_mut() {
-                *el = self.proj(el, &state_mod, Some(sbox)).unwrap();
+        for row in res_state.iter_mut() {
+            for el in row.iter_mut() {
+                *el = self.proj(el, &state_mod, Some(sbox.clone())).unwrap();
             }
         }
 
@@ -138,18 +138,18 @@ pub trait PhotonGadgets: Fancy {
 
     }
 
-    fn ShiftRows(
+    fn ShiftRows<'a>(
         &mut self, 
-        state: &mut PhotonState<Self::Item>
-    ) -> Result<&PhotonState<Self::Item>, Self::Error> { 
+        state: &'a mut PhotonState<Self::Item>
+    ) -> Result<&'a PhotonState<Self::Item>, Self::Error> { 
             let d = state.dim();
-            let tmp: Vec<Self::Item>;
+            let mut tmp: Vec<Self::Item> = Vec::with_capacity(d);
             for i in 1..d {
                 for j in 0..d {
-                    tmp[j] = state.state_matrix()[j][i];
+                    tmp[j] = state.state_matrix()[i][j].clone();
                 }
                 for j in 0..d { 
-                    state.state_matrix()[j][i] = tmp[(j+i)%d];
+                    state.state_matrix[i][j] = tmp[(j+i)%d].clone();
                 }
             }
             Ok(state)
@@ -160,6 +160,27 @@ pub trait PhotonGadgets: Fancy {
         state: &mut PhotonState<Self::Item>,
         Z: &[u16],
     ) -> Result<&PhotonState<Self::Item>, Self::Error> {
+        let d = state.dim();
         
+        let last_row: Vec<Self::Item> = Vec::with_capacity(d);
+
+        
+
+        let mut res: Self::Item;
+        for i in 0..d {
+            let mut sum = self.cmul(&state.state_matrix()[0][i], Z[0]).unwrap();
+            for j in 1..d{
+                if Z[j]!=1 {
+                   sum = self.add(&sum,&self.cmul(&state.state_matrix()[j][i],Z[j]).unwrap()).unwrap();    
+                }
+            }
+            last_row[i] = sum;
+        }
+
+        state.insert(0, last_row);
+        state.state_matrix.rotate_left(1);
+
+
+        Ok(state)
     }
 }
